45d50ab9c856a5fb3ccf322dd0136cae8c4091cf
Sungpil An <ansungpil1@gmail.com>
2020-12-08 18:27:28 -0500

    Added global variables and if statement under userChoice in script.js

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-// Setting variables for Javascript
+
 var generateBtn = document.querySelector("#generate");
-
+// Setting variables for Javascript
 var upc1 = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
-
 var lpc1 = "abcdefghijklmopqrstuvwxyz";
-
 var n1 = "12345677890";
-
 var s1 = "!@#$%^&*()_+";
+//To store newly chosen password using global variables.
+var newPassword =[];
 
-
+var charLength = parseInt(prompt("Your passwords shoud b"))
 //Input for the user to make decision for the password.
 function userChoice(){
     var upper = confirm("Do you want to include uppercase characters in your password?")
-    var lower = confirm("Do you want to include lowercase charaters in your password?")
+    var lower = confirm("Do you want to include lowercase characters in your password?")
     var number = confirm("Do you want to include symbols in your password?")
     var symbol = confirm("Do you want to include numbers in your password?")
-}
+    //Saving user's input choices to form new password.
+    if (upper === true) {
+      (newPwd.push(upc1));
+    }
+    if (lower === true) {
+      (newPwd.push(lpc1));
+    }
+    if (number === true) {
+      (newPwd.push(n1));
+    }
+    if (symbol === true){
+      (newPwd.push(s1));
+    }
 //Saving user's choices for the questions w/ if statement.
 // Write password to the #password input
 function writePassword() {
