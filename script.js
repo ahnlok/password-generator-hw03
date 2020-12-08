3b6580faf65bb2cbf2a0afceb00300cf07b09b71
Sungpil An <ansungpil1@gmail.com>
2020-12-08 18:16:03 -0500

    Added function user choice

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-// Assignment Code
+// Setting variables for Javascript
 var generateBtn = document.querySelector("#generate");
 
+var upc1 = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
+
+var lpc1 = "abcdefghijklmopqrstuvwxyz";
+
+var n1 = "12345677890";
+
+var s1 = "!@#$%^&*()_+";
+
+
+//Input for the user to make decision for the password.
+function userChoice(){
+    var upper = confirm("Do you want to include uppercase characters in your password?")
+    var lower = confirm("Do you want to include lowercase charaters in your password?")
+    var number = confirm("Do you want to include symbols in your password?")
+    var symbol = confirm("Do you want to include numbers in your password?")
+}
+//Saving user's choices for the questions w/ if statement.
 // Write password to the #password input
 function writePassword() {
+  var length = "";
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var upper =["abcdefghijklmopqrstuvwxyz"];
-  var lower =["ABCDEFGHIJKLMOPQRSTUVWXYZ"];
-  var number =["12345677890"];
-  var symbol = ["!@#$%^&*()_+"];
   
-  for (i = 0; i < 8; i++) {
-    var upper = Math.floor(Math.random() * 26);
+ 
 
   }
 
