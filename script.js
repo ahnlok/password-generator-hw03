82a03e13a3aa25671dc7b75d0cda476ff65bb90a
Sungpil An <ansungpil1@gmail.com>
2020-12-09 15:47:43 -0500

    added var charList and completePwd to function passwordLength in script.js

--- script.js
+++ script.js
@@ -1,52 +1,55 @@
 
 var generateBtn = document.querySelector("#generate");
+//Static variables
+var upperC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
 
-var upc1 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lpc1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var n1 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var s1 = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
-
-function generatePassword() {
-  var result = [];
-  var user = "";
-
-  
-
-    var charLength = prompt("How many characters would you like to have between 8 to 128?"); 
+// Function to choose th password length
+function passwordLength() {
     
-    if(charLength < 8 || charLength > 128) {
-      alert("You can only have a length of at least 8 characters and no more than 128 characters");
+  var charList = [];
+  var completePwd = "";
+  
+  var charLength = prompt("How many characters would you like to have between 8 to 128?")
+    if(charLength < 8 || charLength >128){
+      alert("You have to choose password length between 8 and 128 long"); 
     }
+}
+// Function to choose the password combinations
+function generatePassword(){
+    var pwd =  "";
 
     var upperCase = confirm("Do you want to include uppercase characters in your password?");
     var lowerCase = confirm("Do you want to include lowercase characters in your password?");
-    var number = confirm("Do you want to include symbols in your password?");
-    var symbol = confirm("Do you want to include numbers in your password?");
-      
+    var number1 = confirm("Do you want to include symbols in your password?");
+    var symbol1 = confirm("Do you want to include numbers in your password?");
+  
       if (upperCase === true) {
-        (result.push.apply(charLength, upc1));
+        pwd.push(upperC);
       }
       if (lowerCase === true) {
-        (result.push(charLength, lpc1));
+        pwd.push(lowerC);
       }
-      if (number === true) {
-        (result.push(charLength, n1));
+      if (number1 === true) {
+        pwd.push(number);
       }
-      if (symbol === true) {
-        (result.push(charLength, s1));
+      if (symbol1 === true) {
+        pwd.push(symbol);
       }
-      if (charLength.length === 0){
-        alert("Type some characters to proceed with the password generator! \nStarting Over.");
+
+    var completePassword = [];
+    
+      for(var i =0; i < combinations.length; i++) {
+        var random = Math.floor(Math.random() * Math.floor(pwd.length));
+        completePassword.push(pwd[random]);
       }
-      else{
-        for (var i = 0; i < charLength; i ++) {
-        var random = Math.floor(Math.random() * result.length);
-        user += result[random];
-        }
-      }
+     
+      var complete = completePassword.join("");
       document.getElementById("password").innerHTML = resultPass;
-  }
 }
+
 function copyToClipboard() {
   document.getElementById("display").querySelector()
   document.execCommand("Copy");
