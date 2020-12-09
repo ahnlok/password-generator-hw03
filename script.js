a166a77da9d5627efad42deaa2d566643493ece9
Sungpil An <ansungpil1@gmail.com>
2020-12-08 20:01:33 -0500

    Added copytoclipboard in script.js

--- script.js
+++ script.js
@@ -1,42 +1,57 @@
 
 var generateBtn = document.querySelector("#generate");
 
+var upc1 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lpc1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var n1 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var s1 = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
+
 function generatePassword() {
-  var upc1 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var lpc1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var n1 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var s1 = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
-
   var result = [];
-  var user = [];
-
-    var upper = confirm("Do you want to include uppercase characters in your password?");
-    var lower = confirm("Do you want to include lowercase characters in your password?");
-    var number = confirm("Do you want to include symbols in your password?");
-    var symbol = confirm("Do you want to include numbers in your password?");
-      //Saving user's input choices to form new password.
-      if (upper === true) {
-        (newPwd.push(upc1));
-      }
-      if (lower === true) {
-        (newPwd.push(lpc1));
-      }
-      if (number === true) {
-        (newPwd.push(n1));
-      }
-      if (symbol === true) {
-        (newPwd.push(s1));
-      }
-    }
-function writePassword(){
-
-}
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  var user = "";
 
   
-  passwordText.value = password;
-  return "password"
+
+    var charLength = prompt("How many characters would you like to have between 8 to 128?"); 
+    
+    if(charLength < 8 || charLength > 128) {
+      alert("You can only have a length of at least 8 characters and no more than 128 characters");
+    }
+
+    var upperCase = confirm("Do you want to include uppercase characters in your password?");
+    var lowerCase = confirm("Do you want to include lowercase characters in your password?");
+    var number = confirm("Do you want to include symbols in your password?");
+    var symbol = confirm("Do you want to include numbers in your password?");
+      
+      if (upperCase === true) {
+        (result.push.apply(charLength, upc1));
+      }
+      if (lowerCase === true) {
+        (result.push(charLength, lpc1));
+      }
+      if (number === true) {
+        (result.push(charLength, n1));
+      }
+      if (symbol === true) {
+        (result.push(charLength, s1));
+      }
+      if (charLength.length === 0){
+        alert("Type some characters to proceed with the password generator! \nStarting Over.");
+      }
+      else{
+        for (var i = 0; i < charLength; i ++) {
+        var random = Math.floor(Math.random() * result.length);
+        user += result[random];
+        }
+      }
+      document.getElementById("password").innerHTML = resultPass;
+  }
+}
+function copyToClipboard() {
+  document.getElementById("display").querySelector()
+  document.execCommand("Copy");
+  alert("Password copied to your clipboard");
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+copyToClipBoardButton.addEventListener("click", copyToClipboard);
